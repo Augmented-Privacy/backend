80e100bedd2bf277f42668ff0c61342ca6b22cc9
Effi Bennekers <efraim.bennekers@ing.nl>
2018-03-21 18:23:16 +0100

    ok

--- src/main/java/ap/hackathon/augmentedprivacy/generator/TransactionGenerator.java
+++ src/main/java/ap/hackathon/augmentedprivacy/generator/TransactionGenerator.java
@@ -13,11 +13,11 @@ public class TransactionGenerator {
         return transaction;
     }
 
-    private final List<String> SUPERMARKTEN = Arrays.asList(new String[]{"Albert Heijn", "Super de Boer", "Deen", "Jumbo", "Aldi", "Boni", "Dekamarkt", "Lidl", "Hoogvliet", "Konmar", "Maxis", "Makro", "Carrefour", "Colruyt", "Delhaize", "Prima", "Spar" });
-    private final List<String> MODEWINKELS = Arrays.asList(new String[]{"Bomont", "Bonprix", "Bristol", "C&A", "Calvin Klein", "Chasin", "Christine le Duc", "CoolCat", "De Bijenkorf", "Duetz", "H&M", "Hema", "KinderKleding", "Mango", "Miss Etam"});
-    private final List<String> TELECOMBEDRIJVEN = Arrays.asList(new String[]{"KPN", "telecom", "telekom", "T-Mobile", "telfort", "youfone", "Hollandsnieuwe", "Ben", "Orange", "Vodafone", "Simpel", "Delta",});
-    private final List<String> NUTSBEDRIJVEN = Arrays.asList(new String[]{"Qurrent", "Powerpeers", "Eneco", "Oxxio", "Budget Energie", "Essent", "Nuon", "E.ON", "Engie", "Energie direct"});
-    private final List<String> FAMILIELEDEN = Arrays.asList(new String[]{"tante", "oom", "ome", "nonkel", "Mama", "mamma", "pappa", "papa", "oma", "opa", "neef", "nicht", ""});
+    private final List<String> SUPERMARKTEN = Arrays.asList("Albert Heijn", "Super de Boer", "Deen", "Jumbo", "Aldi", "Boni", "Dekamarkt", "Lidl", "Hoogvliet", "Konmar", "Maxis", "Makro", "Carrefour", "Colruyt", "Delhaize", "Prima", "Spar");
+    private final List<String> MODEWINKELS = Arrays.asList("Bomont", "Bonprix", "Bristol", "C&A", "Calvin Klein", "Chasin", "Christine le Duc", "CoolCat", "De Bijenkorf", "Duetz", "H&M", "Hema", "KinderKleding", "Mango", "Miss Etam");
+    private final List<String> TELECOMBEDRIJVEN = Arrays.asList("KPN", "telecom", "telekom", "T-Mobile", "telfort", "youfone", "Hollandsnieuwe", "Ben", "Orange", "Vodafone", "Simpel", "Delta");
+    private final List<String> NUTSBEDRIJVEN = Arrays.asList("Qurrent", "Powerpeers", "Eneco", "Oxxio", "Budget Energie", "Essent", "Nuon", "E.ON", "Engie", "Energie direct");
+    private final List<String> FAMILIELEDEN = Arrays.asList("tante", "oom", "ome", "nonkel", "Mama", "mamma", "pappa", "papa", "oma", "opa", "neef", "nicht", "");
     private final List<String> HUUR = Arrays.asList("Huur","Servicekosten huur","Contributie woningbouwvereniging","Boeteheffing","Achterstallige huur","Rente over achterstallige huur");
     private final List<String> HYPOTHEEKAFSCHRIJVINGEN = Arrays.asList("Afschrijving hypotheek","Premie hypotheek","Bijdrage VvE","Extra storting hypotheek","Extra aflossing hypotheek");
     private final List<String> BELASTINGEN = Arrays.asList("Gemeentebelasting","Hondenbelasting","Wegenbelasting","Boete voor te hard rijden","Inkomstenbelasting","Kansspelbelasting","Erfbelasting");
