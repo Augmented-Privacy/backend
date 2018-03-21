273f8851663cec6f340a71fdbdbf86612b857d7f
Effi Bennekers <efraim.bennekers@ing.nl>
2018-03-21 17:11:30 +0100

    ok

--- src/main/java/ap/hackathon/augmentedprivacy/domain/RetailCustomer.java
+++ src/main/java/ap/hackathon/augmentedprivacy/domain/RetailCustomer.java
@@ -2,9 +2,11 @@ package ap.hackathon.augmentedprivacy.domain;
 
 import jdk.nashorn.internal.objects.annotations.Getter;
 import jdk.nashorn.internal.objects.annotations.Setter;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class RetailCustomer {
 
     private String name;
