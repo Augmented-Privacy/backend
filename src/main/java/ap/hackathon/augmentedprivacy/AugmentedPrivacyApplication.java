711080bacde60250a444296fc213b2c662c3a5b7
ericmanshande <eric.manshande@gmail.com>
2018-03-21 17:02:41 +0100

    jaja

---
+++ src/main/java/ap/hackathon/augmentedprivacy/AugmentedPrivacyApplication.java
@@ -0,0 +1,12 @@
+package ap.hackathon.augmentedprivacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AugmentedPrivacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AugmentedPrivacyApplication.class, args);
+	}
+}
